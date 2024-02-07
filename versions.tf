7a348dcb604fe9c87ed83db3112f76c0f3cb95db
Julian <eng@finisterra.io>
2024-02-06 21:48:31 -0500

    fix: versions

--- versions.tf
+++ versions.tf
@@ -1,18 +1,10 @@
 terraform {
-  required_version = ">= 1.3"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.3"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 1.0"
+      version = ">= 4.67"
     }
   }
 }
